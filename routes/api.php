464aec2ca14e1a35ff5e527c9f66193bc2c4d7bb
Zair <nurmukhamadov.z@future-group.ru>
2020-06-27 22:03:36 +0300

    changed url prefix to /v1

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,19 @@
 <?php
-Route::group(['prefix' => 'auth', 'namespace' => 'Api\V1\Auth'], function() {
-    Route::post('sign-up', 'AuthController@signUp')->name('auth.sign-up');
-    Route::post('sign-in', 'AuthController@signIn')->name('auth.sign-in');
-    Route::post('refresh-token', 'AuthController@refreshToken')->name('auth.refresh-token');
-    Route::get('user', 'AuthController@user')->name('auth.user')->middleware('auth:api');
-    Route::get('verify-email/{user}', 'AuthController@verify')->middleware('signed')->name('auth.verify');
-});
+
+Route::group(['prefix' => 'v1', 'namespace' => 'Api\V1'], function() {
+
+    Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function() {
+        Route::post('sign-up', 'AuthController@signUp')->name('auth.sign-up');
+        Route::post('sign-in', 'AuthController@signIn')->name('auth.sign-in');
+        Route::post('refresh-token', 'AuthController@refreshToken')->name('auth.refresh-token');
+
+        Route::get('user', 'AuthController@user')->name('auth.user')->middleware('auth:api');
+        Route::get('verify-email/{user}', 'AuthController@verify')->middleware('signed')->name('auth.verify');
+    });
 
 
-Route::group(['prefix' => 'v1', 'middleware' => 'auth:api', 'namespace' => 'Api\V1\Todo'], function () {
-
-    Route::apiResource('todo-category', 'TodoCategoryController')->names('todo.category');
-
+    Route::group(['middleware' => 'auth:api', 'namespace' => 'Todo'], function () {
+        Route::apiResource('todo-category', 'TodoCategoryController')->names('todo.category');
+    });
 
 });
